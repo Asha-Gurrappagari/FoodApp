66d4f207a2a228e5f727c5521c23ab0cd115ed29
Asha-Gurrappagari <ashagurrappagari11@gmail.com>
2023-12-08 17:05:55 +0530

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CourseComponent } from './course/course.component';
+import { AboutComponent } from './about/about.component';
+import { JoinnowComponent } from './joinnow/joinnow.component';
+
+const routes: Routes = [
+  {
+    path:'home',component:HomeComponent
+  },
+  {
+    path:'course',component:CourseComponent
+  },
+  {
+    path:'about',component:AboutComponent
+  },
+  {
+    path:'join',component:JoinnowComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { 
+
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { Housinglocation } from '../housinglocation';
+import { CourseComponent } from '../course/course.component';
+import { JoinnowComponent } from '../joinnow/joinnow.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  
+    
+      // id: number= 9999;
+      // name: string = 'Test Home';
+      // city: string = 'Test city';
+      // state:string= 'ST';
+      // photo: string='assets/img/house1.jpg';
+      // availableUnits:number= 99;
+      // wifi:boolean =true;
+      // laundry: boolean=false;
+    
+}
